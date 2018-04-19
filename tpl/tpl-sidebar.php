200f575be8203df741929c682acf243f3080b8f1
felipe huerta iturra --admin1 <felipe.huerta.iturra@gmail.com>
2018-04-19 00:39:16 -0300

    mod: formateo de estructura html

---
+++ tpl/tpl-sidebar.php
@@ -0,0 +1,34 @@
+<div class="sidebar-wrapper">
+    <div class="logo">
+        <a href="#" class="simple-text">
+            Control Stock
+        </a>
+    </div>
+
+    <ul class="nav">
+        <li class="active">
+            <a href="#">
+                <i class="pe-7s-note2"></i>
+                <p>Ingreso MMPP</p>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="pe-7s-note2"></i>
+                <p>Procesado MMPP</p>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="pe-7s-note2"></i>
+                <p>Stock</p>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="pe-7s-graph"></i>
+                <p>Reportes</p>
+            </a>
+        </li>
+    </ul>
+</div>
