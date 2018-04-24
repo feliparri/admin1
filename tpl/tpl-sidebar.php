7b8175727e367c441c4c55f74f1ab895adeb9cd1
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2018-04-24 19:04:28 -0400

    fix: botones sidebar

--- tpl/tpl-sidebar.php
+++ tpl/tpl-sidebar.php
@@ -7,30 +7,23 @@
 	    </div>
 
 	    <ul class="nav">
-	        <li onclick="javascript:accion('MMPP_1_Listar');" class="ButIngresar btn btn-lg btn-primary btn-block btn-signin" >
-	            <a>
-	                <i class="pe-7s-note2"></i>
+	        <li /*class="active"*/ >
+	            <!--<a>
+	                
 	                <p >Ingreso MMPP</p>
-	            </a>
-	            <!--<button  class="ButIngresar btn btn-lg btn-primary btn-block btn-signin" type="submit">Ingreso MMPP</button>-->
+	            </a>-->
+	            <button onclick="javascript:accion('MMPP_1_Listar');" class="ButIngresar btn btn-lg btn-block btn-signin" type="submit"><i class="pe-7s-note2"></i>Ingreso MMPP</button>
 	        </li>
 	        <li class="eventTouch">
-	            <a href="#">
-	                <i class="pe-7s-note2"></i>
-	                <p>Procesado MMPP</p>
-	            </a>
+	            <button onclick="javascript:accion('formInicio_1_FormularioInicio');" class="ButIngresar btn btn-lg btn-block btn-signin" type="submit"><i class="pe-7s-note2"></i>Procesado MMPP</button>
 	        </li>
 	        <li class="eventTouch">
-	            <a href="#">
-	                <i class="pe-7s-note2"></i>
-	                <p>Stock</p>
-	            </a>
+	            <button onclick="javascript:accion('formInicio_1_FormularioInicio');" class="ButIngresar btn btn-lg btn-block btn-signin" type="submit"><i class="pe-7s-note2"></i>Stock</button>
 	        </li>
-	        <li class="eventTouch" onclick="accion('Reportes_1_Listar');">
-	            <a href="#">
-	                <i class="pe-7s-graph"></i>
-	                <p>Reportes</p>
-	            </a>
+	        <li>            
+                <!--<i class="pe-7s-graph"></i>
+                <p>Reportes</p>-->
+                <button onclick="javascript:accion('Reportes_1_Listar');" class="ButIngresar btn btn-lg btn-block btn-signin" type="submit"><i class="pe-7s-graph"></i>Reportes</button>
 	        </li>
 	    </ul>
 	</div>
